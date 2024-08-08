73dc035c59b6ea54c9cf174b474c87da539a74c5
muhaimin nur <you@example.com>
2024-08-08 09:34:28 +0800

    redesign

--- src/Common/generateStylesheetsObject.js
+++
@@ -1,7 +0,0 @@
-export default function generateStylesheetObject(stylesheets) {
-  return stylesheets.map((stylesheet) => ({
-    rel: 'stylesheet',
-    url: stylesheet,
-    precedence: 'default',
-  }));
-}
--- src/Common/removeSlashFromPagination.js
+++
@@ -1,8 +0,0 @@
-const removeSlashFromPagination = () => {
-  let swiperPagination = document.querySelector(".swiper-pagination");
-  if (swiperPagination) {
-    swiperPagination.innerHTML = swiperPagination.innerHTML.replace(" / ", "");
-  }
-};
-
-export default removeSlashFromPagination;
